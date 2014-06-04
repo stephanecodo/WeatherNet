fc2060e93c5a08a553963a57069598c5feef5f41
Stéphane Seigneurin <s.seigneurin@arkadin.com>
2014-06-04 15:59:33 +0200

    Update Weather.cs
    Adding the icon field.

--- WeatherNet/Model/Weather.cs
+++ WeatherNet/Model/Weather.cs
@@ -65,5 +65,10 @@ namespace WeatherNet.Model
         ///     Wind speed in mps.
         /// </summary>
         public Double WindSpeed { get; set; }
+        
+        /// <summary>
+        ///     Icon name.
+        /// </summary>
+        public String Icon { get; set; }
     }
 }
